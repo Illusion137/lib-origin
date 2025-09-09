f21aa366efcac8eb659ecdbd2a38aa8e13d2bc73
Illusion137 <raygoza.d@hotmail.com>
2025-09-09 00:26:52 -0700

    prefs

--- roze/src/prefs.ts
+++ roze/src/prefs.ts
@@ -1,17 +1,16 @@
-import { base_load_map, base_save_map, create_mmkv, generic_load_prefs, generic_reset_prefs, generic_save_pref, type BasePref } from "@native/mmkv/mmkv_utils";
+import { base_load_map, base_save_map, generic_load_prefs, generic_reset_prefs, generic_save_pref, type BasePref } from "@native/mmkv/mmkv_utils";
 import { CookieJar } from "@common/utils/cookie_util";
 import { fs } from "@native/fs/fs";
 import { gen_uuid } from "@common/utils/util";
-import type { MMKVModule } from "@native/mmkv/mmkv.base";
+import { mmkv } from "@native/mmkv/mmkv";
 
 export namespace RozePrefs {
     export type OtherPrefTypes = string & {};
     export type PossibleThemes = keyof typeof themes;
     const roze_mmkv_path = fs().document_directory(".roz");
-    export let mmkv_module: MMKVModule;
 
-    export async function load_mmkv_module(){
-        mmkv_module = create_mmkv({
+    export async function load_mmkv(){
+        await mmkv().load_mmkv({
             id: 'roze.illusion.com',
             path: await roze_mmkv_path
         });
@@ -36,16 +35,16 @@ export namespace RozePrefs {
 
     export function get_pref<T extends PrefOptions>(pref_key: T): (typeof prefs)[T]['default_value'] { return prefs[pref_key].current_value; }
 
-    export function load_prefs() {
-        const storage_user_uuid = mmkv_module.get_string(user_uuid_key);
-        if(storage_user_uuid === null) mmkv_module.set_string(user_uuid_key, user_uuid);
-        generic_load_prefs<OtherPrefTypes, PrefOptions>(mmkv_module, prefs, base_load_map);
+    export async function load_prefs() {
+        const storage_user_uuid = await mmkv().get_string(user_uuid_key);
+        if(storage_user_uuid === null) await mmkv().set_string(user_uuid_key, user_uuid);
+        await generic_load_prefs<OtherPrefTypes, PrefOptions>(mmkv(), prefs, base_load_map);
     }
-    export function save_pref<T extends PrefOptions>(pref_key: T, value: (typeof prefs)[T]['default_value']) {
-        generic_save_pref<OtherPrefTypes, PrefOptions>(mmkv_module, prefs, pref_key, value, base_save_map);
+    export async function save_pref<T extends PrefOptions>(pref_key: T, value: (typeof prefs)[T]['default_value']) {
+        await generic_save_pref<OtherPrefTypes, PrefOptions>(mmkv(), prefs, pref_key, value, base_save_map);
     }
-    export function reset_prefs() {
-        generic_reset_prefs(mmkv_module, [user_uuid_key], load_prefs);
+    export async function reset_prefs() {
+        await generic_reset_prefs(mmkv(), [user_uuid_key], load_prefs);
     }
 
     const themes = {
