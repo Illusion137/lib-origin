e87c88c6a4821294b5161893d4d0c707d45498d1
Illusion137 <raygoza.d@hotmail.com>
2026-05-17 20:42:34 -0700

    chore: need this as well for p2p

--- common/utils/util.ts
+++ common/utils/util.ts
@@ -16,6 +16,9 @@ export function gen_uuid(): string {
 export function decode_hex(hex: string) {
     return hex.replace(/\\x22/g, '"').replace(/\\x7b/g, '{').replace(/\\x7d/g, '}').replace(/\\x5b/g, '[').replace(/\\x5d/g, ']').replace(/\\x3b/g, ';').replace(/\\x3d/g, '=').replace(/\\x27/g, '\'').replace(/\\\\/g, 'doubleAntiSlash').replace(/\\/g, '').replace(/doubleAntiSlash/g, '\\')
 }
+export function gen_small_id(){
+    return gen_uuid().slice(0,8);
+}
 
 export function small_string(str: string, length = 50) {
     if (str.length < length) return str;
