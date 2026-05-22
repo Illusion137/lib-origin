6f216266cdc205342d6e905fd62bb22a0a86a738
Illusion137 <raygoza.d@hotmail.com>
2026-05-22 00:03:32 -0700

    fix: vibes sampler :3

--- Illusive/src/vibes_sampler.ts
+++ Illusive/src/vibes_sampler.ts
@@ -6,20 +6,20 @@ import { generror_catch } from "@common/utils/error_util";
 import { SQLTracks } from "./sql/sql_tracks";
 
 export namespace VibesSampler {
-    export async function predict_track(track: Track){
-        if(is_empty(track.media_uri)) return null;
-        if(track.acousticness !== undefined && track.acousticness !== 0) return null;
+    export async function predict_track(track: Track) {
+        if (is_empty(track.media_uri)) return null;
+        if (track.acousticness !== undefined && track.acousticness !== 0) return null;
         try {
-            const prediction_result = await predict(SQLfs.media_directory(track.media_uri!));
+            const prediction_result = await predict(SQLfs.media_directory(track.media_uri!).replace(/^file:\/\//, ''));
             return prediction_result;
         }
-        catch(e) {
+        catch (e) {
             return generror_catch(e, "vibes sampler failed", "MEDIUM");
         }
     }
-    export async function predict_track_save_result(track: Track){
+    export async function predict_track_save_result(track: Track) {
         const prediction_result = await predict_track(track);
-        if(prediction_result === null || ("error" in prediction_result)) return;
+        if (prediction_result === null || ("error" in prediction_result)) return;
         const new_track: Track = {
             ...track,
             ...prediction_result
