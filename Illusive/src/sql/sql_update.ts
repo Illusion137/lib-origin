a82b2dbb92baef418373b0aebc03b27e39db4170
Illusion137 <raygoza.d@hotmail.com>
2026-06-05 20:52:19 -0700

    chore: let the user know the app is forever updating

--- Illusive/src/sql/sql_update.ts
+++ Illusive/src/sql/sql_update.ts
@@ -231,6 +231,7 @@ export namespace SQLUpdate {
             const tracks = await db.select().from(tracks_table).where(
                 and(ne(tracks_table.youtube_id, ""), eq(tracks_table.artwork_url, ""), ne(tracks_table.thumbnail_uri, ""))
             );
+            GLOBALS.global_var.bottom_alert(`Don't close out of app, must fix ${tracks.length} tracks`, "INFO");
             for (const track of tracks) {
                 const full_path = SQLfs.thumbnail_directory(track.thumbnail_uri);
                 try {
