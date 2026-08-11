77be12234e5747b3e0d313953f2c3394b196533a
Illusion137 <raygoza.d@hotmail.com>
2026-08-10 21:21:28 -0700

    feat(Native FFPROBE): add log support

--- roze/native/ffprobe/ffprobe.node.ts
+++ roze/native/ffprobe/ffprobe.node.ts
@@ -4,20 +4,21 @@ import type { DataCallback, FFPROBE } from "@native/ffprobe/ffprobe.base";
 export const node_ffprobe: FFPROBE = {
     execute_args: async (args: string[], data_callback?: DataCallback) => {
         const ffprobe_spawn = spawn("ffprobe", args);
+        let logs = "";
         const ffprobe_cmd_instance = new Promise<number>((resolve, _) => {
             ffprobe_spawn.stderr?.on('data', (data) => {
                 const output: string = data.toString();
+                logs += output;
                 data_callback?.(output);
             })
             ffprobe_spawn.on('close', (code) => resolve(code ?? 1));
             ffprobe_spawn.on('exit', (code) => resolve(code ?? 1));
         });
 
-        // TODO update logs
         return {
             retcode: ffprobe_cmd_instance,
             session_id: ffprobe_spawn.pid ?? -1,
-            logs: async () => ""
+            logs: async () => logs
         };
     }
 };
